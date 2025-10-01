6c0752765c898a9a8cbea0343b744676d9e133c0
MUHAMMAD63333 <kamranaslam633@gmail.com>
2025-10-01 12:18:06 -0400

    Initial Commit

---
+++ IOSAPP2/IOSAPP2App.swift
@@ -0,0 +1,17 @@
+//
+//  IOSAPP2App.swift
+//  IOSAPP2
+//
+//  Created by Kamran Aslam on 2025-10-01.
+//
+
+import SwiftUI
+
+@main
+struct IOSAPP2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOSAPP2Tests/IOSAPP2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  IOSAPP2Tests.swift
+//  IOSAPP2Tests
+//
+//  Created by Kamran Aslam on 2025-10-01.
+//
+
+import Testing
+@testable import IOSAPP2
+
+struct IOSAPP2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
